8ef56f19d8f5b2caa23b4acaef58034993f220e6
hami <hochstrasser.m@gmail.com>
2019-04-08 10:18:53 +0200

    Install Semantic-UI

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <button className="ui button">Click Here</button>
         </header>
       </div>
     );
