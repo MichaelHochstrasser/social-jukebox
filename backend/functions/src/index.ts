005e2e220ef2b87d2914e83e1fe144770b04b539
Jan Azzati <jazz@Jans-MacBook-Pro.local>
2019-04-08 12:36:53 +0200

    Cloud Functions Template

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,15 +1,3 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import helloWorldFunction from "./helloworld";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-admin.initializeApp();
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+export const helloWorld = helloWorldFunction;
