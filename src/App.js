f6e82202368fbca28983af1fe48f6eeb18993362
Dominic Mülhaupt <dominicmh@gmail.com>
2019-04-08 16:12:32 +0300

    Add routing.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import './App.css';
 import EventInvitation from './components/host/EventInvitation'
 import HostNewEvent from './components/host/HostNewEvent'
 import Playlist from './components/Playlist'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Container from "semantic-ui-react/dist/es/elements/Container/Container";
 
 class App extends Component {
 
   render() {
     return (
-        <div>
-            <Container>
-                <HostNewEvent />
-                <EventInvitation eventName="Sofia's Super Sweet 16" eventLink="https://spotify.com" />
 
-                <Playlist/>
-            </Container>
+        <div>
+          <Router>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/host">Host Event</Link></li>
+              <li><Link to="/host/event">Invite To Event</Link></li>
+              <li><Link to="/event">Go To Event</Link></li>
+            </ul>
+
+            <Route exact path="/" component={HostNewEvent} />
+            <Route exact path="/host" component={HostNewEvent} />
+            <Route exact path="/host/event" render={ (props) => <EventInvitation {...props} eventName="Sofia's Super Sweet 16" eventLink="https://spotify.com" /> } />
+            <Route path="/event" component={Playlist} />
+          </Router>
         </div>
     );
   }
