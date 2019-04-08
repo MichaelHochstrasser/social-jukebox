56f1c27813d1605bac3023b70c722616ea4f6dc8
hami <hochstrasser.m@gmail.com>
2019-04-08 15:24:18 +0200

    Add PlaySite

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import HostNewEvent from './components/host/HostNewEvent'
 import Playlist from './components/playlist/Playlist'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Container from "semantic-ui-react/dist/es/elements/Container/Container";
-import {Playheader} from "./components/playheader/Playheader";
+import {Playheader} from "./components/playlist/Playheader";
+import {PlaySite} from "./components/playlist/PlaySite";
 
 class App extends Component {
 
@@ -24,7 +25,7 @@ class App extends Component {
             <Route exact path="/" component={HostNewEvent} />
             <Route exact path="/host" component={HostNewEvent} />
             <Route exact path="/host/event" render={ (props) => <EventInvitation {...props} eventName="Sofia's Super Sweet 16" eventLink="https://spotify.com" /> } />
-            <Route path="/event" component={Playlist} />
+            <Route path="/event" component={PlaySite} />
           </Router>
         </div>
     );
