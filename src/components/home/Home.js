487ab712c0d11567d139db59696604443d2e7066
Dominic Mülhaupt <dominicmh@gmail.com>
2019-04-08 17:13:51 +0300

    List available events.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,37 @@
+import firebase from "../firebase/Firebase";
 import React, {Component} from 'react';
-import {Redirect} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
+import PlaylistItem from "../playlist/Playlist";
+import {Header} from "semantic-ui-react";
 
 export class Home extends Component {
-    state = {
-        redirect: false
+
+    constructor(props) {
+        super(props);
+
+        this.db = firebase.firestore().collection('Events');
     }
+
+    state = {
+        redirect: false,
+        events: []
+    }
+
+    componentDidMount() {
+        this.db.onSnapshot(this.onUpdate )
+    }
+
+    onUpdate = (querySnapshot) => {
+        const events = [];
+        querySnapshot.forEach((doc) => {
+            const { name } = doc.data();
+            events.push({
+                key: doc.id,
+                name: name
+            });
+        });
+        this.setState({events});
+    };
 
     onCreateEvent = () => {
         this.setState({
@@ -27,10 +54,15 @@ export class Home extends Component {
     }
 
     render() {
+
         return <div>
             {this.renderRedirect()}
             <button onClick={this.onCreateEvent}>Create event</button>
-            <button onClick={this.onSearch}>Search & Join event</button>
+            <button onClick={this.onSearch}>Example Event</button>
+            <Header as='h1'>Current Events</Header>
+            <ul>
+                {this.state.events.map(event => <li key={event.key}><Link to='/event/:id'>{event.name}</Link></li>)}
+            </ul>
         </div>
     }
 }
