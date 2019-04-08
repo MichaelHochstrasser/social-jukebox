ae7d5d93c0d6450468a9413aabfef37203e24330
Jan Azzati <jazz@Jans-MacBook-Pro.local>
2019-04-08 17:08:30 +0300

    addSong Endpoint

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,6 +1,9 @@
 import helloWorldFunction from "./helloworld";
 import createEventFunction from "./createevent";
+import addSongFunction from "./addsong";
 
 export const helloWorld = helloWorldFunction;
 
 export const createEvent = createEventFunction;
+
+export const addSong = addSongFunction;
--- backend/functions/src/model/Song.ts
+++ backend/functions/src/model/Song.ts
@@ -3,10 +3,10 @@ import { Voter } from "./Voter";
 export class Song {
   constructor(
     public eventId: string,
+    public songId: string = "spotify:track:6rqhFgbbKwnb9MLmUQDhG6",
     public dateAdded: Date = new Date(),
     public isPlaying: boolean = false,
     public voters: Voter[] = [],
-    public voteCount: number = 0,
-    public songId?: string
+    public voteCount: number = 0
   ) {}
 }
