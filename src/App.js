79b45a938276fdd350593ecac13f3b53f6ba1e19
s-mueller <mail@sebastian-mueller.li>
2019-04-08 14:44:00 +0300

    Add search modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import HostNewEvent from './components/host/HostNewEvent'
-import { Grid } from 'semantic-ui-react'
 import Playlist from './components/Playlist'
 
 class App extends Component {
