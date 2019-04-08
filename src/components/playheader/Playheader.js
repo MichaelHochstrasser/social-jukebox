f9aa6f6ad0080143f3862186e0a0b10aaeafa61f
hami <hochstrasser.m@gmail.com>
2019-04-08 15:15:32 +0200

    Add PlayerHeader

--- src/components/playheader/Playheader.js
+++ src/components/playheader/Playheader.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import {Container, Grid, Icon, Progress, Segment} from "semantic-ui-react";
 
 export class Playheader extends Component {
 
     render() {
-        return <div>
-            <h1>Current Song Playing</h1>
-        </div>
+        return <Segment>
+            <Grid className="App" columns={1}>
+                <Grid.Row>
+                    <Container textAlign='center'>
+                        <h2>Hit me baby one more time</h2>
+                        <p>Britney Spears</p>
+                        <Icon name='play' />
+                    </Container>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Progress percent={10} size='tiny'>
+                            4:30
+                        </Progress>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Segment>
     }
 }
