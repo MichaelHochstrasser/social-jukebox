41b8fcd00c09f058833e1b00e5b15684f63b030c
hami <hochstrasser.m@gmail.com>
2019-04-08 16:44:55 +0200

    Add Constructor to Playheader

--- src/components/playlist/Playheader.js
+++ src/components/playlist/Playheader.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import {Container, Grid, Icon, Progress, Segment} from "semantic-ui-react";
+import firebase from "../firebase/Firebase";
 
 export class Playheader extends Component {
+
+    constructor(props) {
+        super(props);
+        this.db = firebase.firestore().collection('test');
+        const song = {title: String, artist: String, time: String};
+        this.state = {
+            currentSong: {title: 'Hit me baby one more time', artist: 'Britney', time: '2:15'}
+        };
+    }
 
     render() {
         return <Segment>
             <Grid className="App" columns={1}>
                 <Grid.Row>
                     <Container textAlign='center'>
-                        <h2>Hit me baby one more time</h2>
+                        <h2>{this.state.currentSong.title}</h2>
                         <p>Britney Spears</p>
                         <Icon name='play' />
                     </Container>
