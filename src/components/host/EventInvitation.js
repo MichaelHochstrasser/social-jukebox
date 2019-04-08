f665146f66b623a14b69ed8542f07d6383a02788
Dominic Mülhaupt <dominicmh@gmail.com>
2019-04-08 14:09:39 +0200

    Beautify QR code.

--- src/components/host/EventInvitation.js
+++ src/components/host/EventInvitation.js
@@ -7,7 +7,12 @@ export default (props) => {
     <Grid centered columns={1}>
         <Grid.Row><Header as='h1'>{props.eventName}</Header></Grid.Row>
         <Grid.Row><a href={props.eventLink}>{props.eventLink}</a></Grid.Row>
-        <Grid.Row><QRCode value={props.eventLink} size={256} /></Grid.Row>
+        <Grid.Row><QRCode
+          value={props.eventLink}
+          size={256}
+          logo="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Love_Heart_symbol.svg/2000px-Love_Heart_symbol.svg.png"
+          bgColor="#2ED665"
+          fgColor="#000000"/></Grid.Row>
     </Grid>
   )
 }
