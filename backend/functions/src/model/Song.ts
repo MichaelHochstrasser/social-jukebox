597c74998a9c1b1fbb7a15bb043a882ae0dd607d
Jan Azzati <jazz@Jans-MacBook-Pro.local>
2019-04-09 11:32:06 +0300

    getting song infos when adding songs to playlists

--- backend/functions/src/model/Song.ts
+++ backend/functions/src/model/Song.ts
@@ -1,11 +1,20 @@
 import { Voter } from "./Voter";
 
-export class Song {
+import { SpotifyTrack } from "./SpotifyTrack";
+
+export class Song implements SpotifyTrack {
+  public get id() {
+    return this.songId;
+  }
+
   constructor(
     public eventId: string,
     public songId: string = "6rqhFgbbKwnb9MLmUQDhG6",
-    public title: string = "",
+    public name: string = "",
     public artist: string = "",
+    public duration_ms: number = 0,
+    public popularity: number = 0,
+    public image: string = "",
     public dateAdded: Date = new Date(),
     public isPlaying: boolean = false,
     public voters: Voter[] = [],
--- backend/functions/src/shared/SpotifyApiHelper.ts
+++ backend/functions/src/shared/SpotifyApiHelper.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
+import { SpotifyTrack } from "../model/SpotifyTrack";
+
 import { createSpotifyAPITrackURL } from "./constants";
 
 export function createHeader(
@@ -20,16 +22,27 @@ export function createHeader(
 export class SpotifyHelper {
   constructor(private spotifyToken?: string) {}
 
-  getSongInfo(
-    songId: string
-  ): Promise<{ title: string; artist: string } | void> {
+  getSongInfo(songId: string): Promise<SpotifyTrack | void> {
     const requestUrl = createSpotifyAPITrackURL(songId);
 
     if (this.spotifyToken) {
       return axios
         .get(requestUrl, createHeader(this.spotifyToken))
         .then((response: any) => {
-          console.log(response);
+          if (response.data) {
+            return {
+              id: response.data.id,
+              name: response.data.name,
+              duration_ms: response.data.duration_ms,
+              artist: response.data.artists
+                .map((artist: { name: string }) => artist.name)
+                .join(","),
+              popularity: response.data.popularity,
+              image: response.data.album.images[0].url
+            } as SpotifyTrack;
+          } else {
+            throw new Error("No data returned!");
+          }
         })
         .catch(err => {
           throw err;
