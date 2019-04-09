c19881e10fb8f8e1f8f81f8c10ad68a698a20629
Michael <HighFish87@gmail.com>
2019-04-09 16:36:40 +0200

    add functionality to request new access tokens with refresh tokens

--- backend/functions/src/shared/FirestoreHelper.ts
+++ backend/functions/src/shared/FirestoreHelper.ts
@@ -32,7 +32,7 @@ export class FireStoreHelper {
       if (event && event.playlistId) {
         resolve(event.playlistId);
       } else if (event.spotifyToken) {
-        const spotifyHelper = new SpotifyHelper(event.spotifyToken);
+        const spotifyHelper = new SpotifyHelper(event.spotifyToken, event.refreshToken, event.validUntil);
 
         spotifyHelper
           .createPlaylist(event.name)
