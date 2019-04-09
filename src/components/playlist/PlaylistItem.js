29e46c8099f67ef6853f97c3c5143b01a711bc32
hami <hochstrasser.m@gmail.com>
2019-04-09 09:59:03 +0200

    Axion Request for Up- and Downvote

--- src/components/playlist/PlaylistItem.js
+++ src/components/playlist/PlaylistItem.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import { Grid, Button, Icon, Label, Segment, Table, Header, Image } from 'semantic-ui-react'
+import { axios } from 'axios'
 
 class PlaylistItem extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    handleVote(vote) {
+        const axios = require('axios');
+
+        const url = 'https://us-central1-social-jukebox-zuehlke.cloudfunctions.net/vote';
+        const body = {
+            songId: 'spotify:track:6rqhFgbbKwnb9MLmUQDhG6',
+            eventId: 'Il9rNPngcXmEbE5UZaZw',
+            vote: vote,
+            sessionId: 'frontendTest1'
+        };
+        const header = {
+            'Content-Type': 'application/json'
+        };
+
+        axios.post(url, body, header)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    };
+
     render() {
         return <Table.Row>
-                <Table.Cell One>
+                <Table.Cell>
                     <Header as='h4' image>
                         <Image src={process.env.PUBLIC_URL + '/images/song2.jpg'} rounded size='mini' />
                         <Header.Content>
@@ -15,9 +44,9 @@ class PlaylistItem extends Component {
                 </Table.Cell>
                 <Table.Cell textAlign='right'>
                     <Button.Group size='mini'>
-                        <Button icon color='red'><Icon name='thumbs down outline' /></Button>
+                        <Button icon color='red' onClick={this.handleVote.bind(this, -1)}><Icon name='thumbs down outline' /></Button>
                         <Button basic color='grey'>{this.props.votes}</Button>
-                        <Button icon color='green'><Icon name='thumbs up outline' /></Button>
+                        <Button icon color='green' onClick={this.handleVote.bind(this, 1)}><Icon name='thumbs up outline' /></Button>
                     </Button.Group>
                 </Table.Cell>
             </Table.Row>
