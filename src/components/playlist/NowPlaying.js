2a19e5b5123e8eb4a31eece2c4ebed56925b454a
Dominic Mülhaupt <dominicmh@gmail.com>
2019-04-11 14:27:35 +0300

    Display now playing song and artist.

--- src/components/playlist/NowPlaying.js
+++ src/components/playlist/NowPlaying.js
@@ -7,74 +7,82 @@ export class NowPlaying extends Component {
     constructor(props) {
         super(props);
         this.spotifyWebPlaybackSDKReady = false;
+        this.player = {};
 
         //this.setUpSpotifyPlayer = this.setUpSpotifyPlayer.bind(this);
         this.state = {
-            currentSong: {title: 'Hit me baby one more time', artist: 'Britney', time: '2:15'},
-            paused: true
+            currentTrack: {},
+            paused: true,
         };
     }
 
-    componentWillMount() {
-        window.onSpotifyWebPlaybackSDKReady = () => this.spotifyWebPlaybackSDKReady = true;
+    componentDidMount() {
+        const spotifyPlayerScriptTag = document.createElement('script');
+        spotifyPlayerScriptTag.src = "https://sdk.scdn.co/spotify-player.js";
+        spotifyPlayerScriptTag.id = "spotifyPlayer";
+        document.body.appendChild(spotifyPlayerScriptTag);
+        window.onSpotifyWebPlaybackSDKReady = () => this.setUpSpotifyPlayer();
+    }
 
-        let eventDocRef = firebase.firestore().collection('Events').doc(this.props.eventId);
+    componentWillUnmount() {
+        this.player.disconnect();
+        let spotifyPlayerScriptyTag = document.getElementById("spotifyPlayer");
+        document.body.removeChild(spotifyPlayerScriptyTag);
+    }
+
+    setUpSpotifyPlayer() {
+        const eventDocRef = firebase.firestore().collection('Events').doc(this.props.eventId);
         eventDocRef.get()
             .then(doc => {
                 let token = doc.data().spotifyToken;
-                if (this.spotifyWebPlaybackSDKReady) {
-                    this.setUpSpotifyPlayer(token)
-                } else {
-                    window.onSpotifyWebPlaybackSDKReady = () => this.setUpSpotifyPlayer(token);
-                }
+                this.player = new window.Spotify.Player({
+                    name: 'Social Jukebox',
+                    getOAuthToken: cb => {
+                        cb(token);
+                    }
+                });
+
+                // Error handling
+                this.player.addListener('initialization_error', ({message}) => console.error(message));
+                this.player.addListener('authentication_error', ({message}) => console.error(message));
+                this.player.addListener('account_error', ({message}) => console.error(message));
+                this.player.addListener('playback_error', ({message}) => console.error(message));
+
+                // Playback status updates
+                this.player.addListener('player_state_changed', state => {
+                    console.log(state);
+                    if (state) {
+                        this.setState({paused: state.paused, currentTrack: state.track_window.current_track});
+                    }
+                });
+
+                // Ready
+                this.player.addListener('ready', ({device_id}) => console.log('Ready with Device ID', device_id));
+
+                // Not Ready
+                this.player.addListener('not_ready', ({device_id}) => console.log('Device ID has gone offline', device_id));
+
+                // Connect to the player!
+                this.player.connect();
             })
             .catch(error => console.log("Error getting document: ", error));
     }
 
-    setUpSpotifyPlayer(token) {
-        const player = new window.Spotify.Player({
-            name: 'Social Jukebox',
-            getOAuthToken: cb => {
-                cb(token);
-            }
-        });
-
-        // Error handling
-        player.addListener('initialization_error', ({message}) => console.error(message));
-        player.addListener('authentication_error', ({message}) => console.error(message));
-        player.addListener('account_error', ({message}) => console.error(message));
-        player.addListener('playback_error', ({message}) => console.error(message));
-
-        // Playback status updates
-        player.addListener('player_state_changed', state => {
-            console.log(state);
-            this.setState({paused: state.paused})
-        });
-
-        // Ready
-        player.addListener('ready', ({device_id}) => console.log('Ready with Device ID', device_id));
-
-        // Not Ready
-        player.addListener('not_ready', ({device_id}) => console.log('Device ID has gone offline', device_id));
-
-        // Connect to the player!
-        player.connect();
-    }
-
     render() {
+        const artistName = this.state.currentTrack.artists ? this.state.currentTrack.artists[0].name : '';
         return <Segment>
             <Grid className="App" columns={1}>
                 <Grid.Row>
                     <Container textAlign='center'>
-                        <h2>{this.state.currentSong.title}</h2>
-                        <p>{this.state.currentSong.artist}</p>
+                        <h2>{this.state.currentTrack.name}</h2>
+                        <p>{artistName}</p>
                         <Icon name={(this.state.paused) ? 'play' : 'pause'} />
                     </Container>
                 </Grid.Row>
                 <Grid.Row>
                     <Grid.Column>
                         <Progress percent={10} size='tiny'>
-                            {this.state.currentSong.time}
+                            {this.state.currentTrack.time}
                         </Progress>
                     </Grid.Column>
                 </Grid.Row>
